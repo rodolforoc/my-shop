4c7143adff5a7b3a206cdeef4bbc6b35d794ee38
rodolforoc <rodolforoc97@gmail.com>
2020-11-30 20:39:40 -0300

    add products grid component

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,13 +1,12 @@
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import '../models/product.dart';
 import '../utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
-  final Product product;
-
-  const ProductItem(this.product);
   @override
   Widget build(BuildContext context) {
+    final Product product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -26,9 +25,12 @@ class ProductItem extends StatelessWidget {
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            icon: Icon(Icons.favorite),
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
             color: Theme.of(context).accentColor,
-            onPressed: () {},
+            onPressed: () {
+              product.toggleFavorite();
+            },
           ),
           title: Text(
             product.title,
