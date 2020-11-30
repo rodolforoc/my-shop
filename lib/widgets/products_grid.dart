55d5a2e671ec4a63201773772504e4b3d41885b3
rodolforoc <rodolforoc97@gmail.com>
2020-11-30 20:50:40 -0300

    add consumer to favorite

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
+import '../widgets/product_item.dart';
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsProvider = Provider.of<Products>(context);
+    final products = productsProvider.items;
+
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: products[i],
+        child: ProductItem(),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
